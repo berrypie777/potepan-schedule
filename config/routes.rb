0d8c44651fee91ee3bbe7738a2a63ece6e0a06de
Kenta Koana <administrator@MacBook-Air.local>
2023-05-23 12:31:42 +0900

    routesを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/show'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/destroy'
+  
   resources :posts
   root to: 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
